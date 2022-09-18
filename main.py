2b27cf101d55785cde7812409d700b5d17f98930
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2022-09-19 02:12:35 +0300

    update

--- main.py
+++ main.py
@@ -7,66 +7,42 @@ class Program:
         self.window = Tk()
         self.window.title('2/10 calculator')
         self.window.resizable(width=False,height=False)
-        """Vars"""
+        """Variables"""
         self.var = IntVar()
         self.var1 = IntVar()
-        self.var2 = BooleanVar()
-        self.var3 = BooleanVar()
-        """Widgets"""
-        self.inpt = Entry(textvariable=self.var, justify=CENTER, font=('Arial', 15))
-        self.inpt.grid(column=0, row=0)
+        self.var2 = IntVar()
+        self.var3 = IntVar()
 
-        self.inpt1 = Entry(textvariable=self.var1, justify=CENTER, font=('Arial', 15))
-        self.inpt1.grid(column=1, row=0)
+    def countBase10(self):
+        """Return base10 int"""
+        return self.var.get() + self.var1.get()
 
-        self.lbl = Label(justify=CENTER, text='Result', wraplength=False, padx=50, font=('Arial', 15))
-        self.lbl.grid(column=2, row=1)
-
-        self.lbl1 = Label(justify=CENTER, text='0', wraplength=False, padx=50, font=('Arial', 15))
-        self.lbl1.grid(column=2, row=2)
-
-        self.rbtn = Radiobutton(text='From base 10', variable=self.var2, value=1, font=('Arial', 15))
-        self.rbtn.grid(column=0, row=1)
-
-        self.rbtn1 = Radiobutton(text='From base 2', variable=self.var2, value=0, font=('Arial', 15))
-        self.rbtn1.grid(column=1, row=1)
-
-        self.rbtn2 = Radiobutton(text='To base 10', variable=self.var3, value=1, font=('Arial', 15))
-        self.rbtn2.grid(column=0, row=2)
-
-        self.rbtn3 = Radiobutton(text='To base 2', variable=self.var3, value=0, font=('Arial', 15))
-        self.rbtn3.grid(column=1, row=2)
-
-        self.countbtn = Button(text='Count result', padx=160, font=('Arial', 15), command=self.count_result)
-        self.countbtn.grid(column=0, row=3, columnspan=2)
-        """Window loop"""
-        self.window.mainloop()
-
-    def count_sum(self):
-        if self.var2.get():
-            """sum from base 10"""
-            return self.var.get() + self.var1.get()
-        else:
-            """sum from base 2"""
-            return self.to_base2(self.to_base10(self.var.get()) + self.to_base10(self.var1.get()))
+    def countBase2(self):
+        """Return base2 int"""
+        return int(self.to_base2(self.to_base10(self.var.get()) + self.to_base10(self.var1.get())))
 
     def count_result(self):
-        if self.var3.get():
-            self.lbl1.config(text=self.count_sum())
-        else:
-            if self.check_base():
-                self.lbl1.config(text=self.to_base2(self.count_sum()))
+        try:
+            """update error status"""
+            self.errlbl.config(text='')
+            """if from base2"""
+            if bool(self.var2.get()):
+                """if to base2"""
+                if bool(self.var3.get()):
+                    self.lbl1.config(text=self.countBase2())
+                else:
+                    self.lbl1.config(text=self.countBase10())
             else:
-                print('NaN')
-
-    def check_base(self):
-        for i in self.var.get(), self.var1.get():
-            print(i)
-            if i != '0' or i != '1':
-                return False
-            return True
+                if bool(self.var3.get()):
+                    self.lbl1.config(text=self.countBase2())
+                else:
+                    self.lbl1.config(text=self.countBase10())
+        except Exception as e:
+            print(e)
+            self.errlbl.config(text="Can't count result.")
 
     def to_base2(self, a):
+        """Int to base2"""
         n = ''
         k = ''
         while a > 0:
@@ -78,6 +54,7 @@ class Program:
         return k
 
     def to_base10(self, a):
+        """Base2 to int"""
         a = str(a)
         k = 0
         for i in range(len(a)):
@@ -85,8 +62,35 @@ class Program:
         return k
 
     def main(self):
-        pass
+        """Widgets"""
+        # left input
+        self.inpt = Entry(textvariable=self.var, justify=CENTER, font=('Arial', 15))
+        self.inpt.grid(column=0, row=0)
+        # right input
+        self.inpt1 = Entry(textvariable=self.var1, justify=CENTER, font=('Arial', 15))
+        self.inpt1.grid(column=1, row=0)
+        # placeholder label
+        self.lbl = Label(justify=CENTER, text='Result', wraplength=False, padx=50, font=('Arial', 15))
+        self.lbl.grid(column=2, row=1)
+        # result label
+        self.lbl1 = Label(justify=CENTER, text='0', wraplength=False, padx=50, font=('Arial', 15))
+        self.lbl1.grid(column=2, row=2)
+        # From int-base2 checkbox
+        self.cbtn = Checkbutton(text='from base 2', variable=self.var2, font=('Arial', 15))
+        self.cbtn.grid(column=0, row=1)
+        # To int-base2 checkbox
+        self.cbtn1 = Checkbutton(text='To base 2', variable=self.var3, font=('Arial', 15))
+        self.cbtn1.grid(column=1, row=1)
+        # Count result button
+        self.countbtn = Button(text='Count result', padx=160, font=('Arial', 15), command=self.count_result)
+        self.countbtn.grid(column=0, row=2, columnspan=2)
+        """Error label"""
+        self.errlbl = Label(justify=CENTER, text='', wraplength=False, font=('Arial', 15))
+        self.errlbl.grid(column=0, row=3)
+        """Window loop"""
+        self.window.mainloop()
 
 
 if __name__ == "__main__":
     x = Program()
+    x.main()
