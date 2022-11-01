8bf88ae8e0b6fc2481f619b36336432a43178bb6
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2022-11-01 20:43:15 +0300

    reworked

--- main.py
+++ main.py
@@ -5,92 +5,75 @@ class Program:
     def __init__(self):
         """Window"""
         self.window = Tk()
-        self.window.title('2/10 calculator')
+        self.window.title('Different base calculator')
         self.window.resizable(width=False,height=False)
         """Variables"""
-        self.var = IntVar()
-        self.var1 = IntVar()
-        self.var2 = IntVar()
-        self.var3 = IntVar()
+        self.num = StringVar()
+        self.num1 = StringVar()
+        self.base = IntVar()
+        self.base1 = IntVar()
+        self.base2 = IntVar()
+        self.operator = True
+        """Widgets"""
+        # first input
+        self.inpt = Entry(textvariable=self.num, justify=CENTER, font=('Arial Bold', 10)).grid(column=0, row=0, padx=5)
+        # second input
+        self.inpt1 = Entry(textvariable=self.num1, justify=CENTER, font=('Arial Bold', 10)).grid(column=2, row=0, padx=5)
+        # first select base
+        self.selectbase = Spinbox(textvariable=self.base, from_=2, to=36, justify=CENTER).grid(column=0, row=1, padx=5)
+        # second select base
+        self.selectbase1 = Spinbox(textvariable=self.base1, from_=2, to=36, justify=CENTER).grid(column=2, row=1, padx=5)
+        # third select base
+        self.selectbase2 = Spinbox(textvariable=self.base2, from_=2, to=36, justify=CENTER).grid(column=0, row=3, padx=5)
+        # result label
+        self.lbl = Label(justify=CENTER, text='Result', font=('Arial Bold', 15))
+        self.lbl.grid(column=1, row=2, columnspan=2, padx=5)
+        # count result
+        self.countbtn = Button(text='Count result', font=('Arial Bold', 11), command=self.count_result)
+        self.countbtn.grid(column=0, row=2, padx=0, ipadx=25)
+        # change base
+        self.basebtn = Button(text='+', font=('Arial Bold', 10), command=self.change_operator)
+        self.basebtn.grid(column=1, row=0, padx=10, ipadx=5)
+        """Error label"""
+        self.errlbl = Label(justify=CENTER, text='', wraplength=False, font=('Arial', 11))
+        self.errlbl.grid(column=1, row=3, columnspan=2)
+        """Run app"""
+        self.window.mainloop()
 
-    def countBase10(self):
-        """Return base10 int"""
-        return self.var.get() + self.var1.get()
+    def change_operator(self):
+        """Changes operator and button text"""
+        if self.operator:
+            self.operator = False
+            self.basebtn.config(text='x')
+        else:
+            self.operator = True
+            self.basebtn.config(text='+')
 
-    def countBase2(self):
-        """Return base2 int"""
-        return int(self.to_base2(self.to_base10(self.var.get()) + self.to_base10(self.var1.get())))
+    def convert_base(self, num, to_base=10, from_base=10):
+        """Converts number from base to base"""
+        n = int(num, from_base) if isinstance(num, str) else num
+        alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        res = ""
+        while n > 0:
+            n, m = divmod(n, to_base)
+            res += alphabet[m]
+        return res[::-1]
 
     def count_result(self):
+        """Counts result of two inputs"""
+        self.errlbl.config(text="")
         try:
-            """update error status"""
-            self.errlbl.config(text='')
-            """if from base2"""
-            if bool(self.var2.get()):
-                """if to base2"""
-                if bool(self.var3.get()):
-                    self.lbl1.config(text=self.countBase2())
-                else:
-                    self.lbl1.config(text=self.countBase10())
+            firstnum = self.convert_base(self.num.get(), 10, self.base.get())
+            secondnum = self.convert_base(self.num1.get(), 10, self.base1.get())
+            if self.operator:
+                self.lbl.config(text=self.convert_base(int(firstnum)+int(secondnum), self.base2.get(), 10))
             else:
-                if bool(self.var3.get()):
-                    self.lbl1.config(text=self.countBase2())
-                else:
-                    self.lbl1.config(text=self.countBase10())
+                self.lbl.config(text=self.convert_base(int(firstnum)*int(secondnum), self.base2.get(), 10))
         except Exception as e:
-            print(e)
             self.errlbl.config(text="Can't count result.")
-
-    def to_base2(self, a):
-        """Int to base2"""
-        n = ''
-        k = ''
-        while a > 0:
-            n = n + str(a % 2)
-            a = a // 2
-        n = list(reversed(n))
-        for j in range(len(n)):
-            k += n[j]
-        return k
-
-    def to_base10(self, a):
-        """Base2 to int"""
-        a = str(a)
-        k = 0
-        for i in range(len(a)):
-            k += int(a[len(a) - i - 1]) * int(2 ** i)
-        return k
-
-    def main(self):
-        """Widgets"""
-        # left input
-        self.inpt = Entry(textvariable=self.var, justify=CENTER, font=('Arial', 15))
-        self.inpt.grid(column=0, row=0)
-        # right input
-        self.inpt1 = Entry(textvariable=self.var1, justify=CENTER, font=('Arial', 15))
-        self.inpt1.grid(column=1, row=0)
-        # placeholder label
-        self.lbl = Label(justify=CENTER, text='Result', wraplength=False, padx=50, font=('Arial', 15))
-        self.lbl.grid(column=2, row=1)
-        # result label
-        self.lbl1 = Label(justify=CENTER, text='0', wraplength=False, padx=50, font=('Arial', 15))
-        self.lbl1.grid(column=2, row=2)
-        # From int-base2 checkbox
-        self.cbtn = Checkbutton(text='from base 2', variable=self.var2, font=('Arial', 15))
-        self.cbtn.grid(column=0, row=1)
-        # To int-base2 checkbox
-        self.cbtn1 = Checkbutton(text='To base 2', variable=self.var3, font=('Arial', 15))
-        self.cbtn1.grid(column=1, row=1)
-        # Count result button
-        self.countbtn = Button(text='Count result', padx=160, font=('Arial', 15), command=self.count_result)
-        self.countbtn.grid(column=0, row=2, columnspan=2)
-        """Error label"""
-        self.errlbl = Label(justify=CENTER, text='', wraplength=False, font=('Arial', 15))
-        self.errlbl.grid(column=0, row=3)
-        """Window loop"""
-        self.window.mainloop()
+            print(e)
 
 
 if __name__ == "__main__":
-    x = Program()
-    x.main()
+     App = Program()
+
